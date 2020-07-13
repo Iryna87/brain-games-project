20f5704789b5cf2497bf6dad8f99050370dd6f02
Irena <iryna.morozova87@gmail.com>
2020-07-13 12:33:10 +0200

    update

--- bin/brain-calc_uten_index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-import * as kopi from '../game/cli_brain_calc_uten_index.js';
--- bin/brain-gcd_uten_index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-import * as gcd from '../game/cli_brain_gcd_uten_index.js';
